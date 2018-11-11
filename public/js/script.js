038ee6c58ec3822d015d649a0bd7e4e92dcdc918
shubham100795 <shubham100795@gmail.com>
2018-11-12 00:58:14 +0530

    Added location feature

--- public/js/script.js
+++ public/js/script.js
@@ -4,15 +4,58 @@ socket.on('connect',function(){
   //socket.emit('newEmailFromClient',{From:'Client',To:'Server'});//create email only when server connected
 });
 
-socket.on('newMessage',function(message){//received from server
-  console.log('New message received',message);
+jQuery(document).ready(function() {
+
+  jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+    socket.emit('createMessage',{
+      From:'User',
+      Text:jQuery('[name=message]').val()
+    });
+    return false;
+  });
+
+  socket.on('newMessage',function(message){//received from server
+    console.log('New message received',message);
+    var li=jQuery('<li></li>');
+    li.text(`From:${message.From}: ${message.Text}`);
+    jQuery('#messages').append(li);
+  });
+
+  var location=jQuery('#location-button');
+  location.on('click',function(){
+    if(!navigator.geolocation){
+      return alert('Geolocation not supported by the browser!');
+    }
+    else {
+      navigator.geolocation.getCurrentPosition(function(position){
+        socket.emit('sendLocation',{
+          latitude:position.coords.latitude,
+          longitude:position.coords.longitude
+        });
+      },function(){
+        return alert('Unable to fetch location');
+      })
+    }
+  });
+
+  socket.on('newLocationMessage',function(message){
+  var li=jQuery("<li></li>");
+  var a=jQuery('<a target="_blank">My current location</a>');
+  li.text(`From:${message.From}:`);
+  a.attr('href',message.Url);
+  li.append(a);
+  jQuery('#messages').append(li);
+  });
+
 });
+
 
 socket.on('disconnect',function(){
   console.log('Disconnected from server');
 });
 
+
 // socket.on('newEmailFromServer',function(data){
 //   console.log('new email received',data);
 // });
-//emit createMessage from console.//sent to server
--- server/server.js
+++ server/server.js
@@ -21,12 +21,22 @@ io.on('connection',(socket)=>{//socket is an object for each indivisual user
   //   console.log('Client sent a new email',data);
   // });
   socket.emit('newMessage',{From:'Admin',Text:'Welcome to the chat app!'});
+
   socket.broadcast.emit('newMessage',{From:'Admin',Text:'New user joined!'});
-  
+
   socket.on('createMessage',(message)=>{
     console.log('User created new message',message);
     io.emit('newMessage',{From:message.From,Text:message.Text,createdAt:new Date()});//emit sends to all users
   });
+
+  socket.on('sendLocation',(position)=>{
+    io.emit('newLocationMessage',{
+      From:'User',
+      Url:`https://www.google.com/maps?q=${position.latitude},${position.longitude}`,
+      createdAt:new Date()
+    });
+  });
+
   socket.on('disconnect',()=>{
     console.log('Disconnected');
   });
